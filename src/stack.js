f16895cdb3abdbd9efd44584a4051b20da4b3765
Yahor Tsytsarkin <egormail88@gmail.com>
2023-04-16 21:58:06 +0300

    feat: stack done

--- src/stack.js
+++ src/stack.js
@@ -14,19 +14,23 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    return this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    // console.log("АУУ");
+    return this.stack.pop();
   }
+  // stack.pop(1);
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    // if (this.stack)
+    return this.stack[this.stack.length - 1];
   }
 }
 
